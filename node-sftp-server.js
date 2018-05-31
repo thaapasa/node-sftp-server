08b03c4fa8e29f2798334f948bf80b758c43b2a8
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2018-05-31 10:05:52 +0300

    Also recognize custom file mode w-

--- node-sftp-server.js
+++ node-sftp-server.js
@@ -372,6 +372,7 @@ var SFTPSession = (function(superClass) {
           return this.sftpStream.handle(reqid, handle);
         }.bind(this));
       case "w":
+      case "w-":
         rs = new Readable();
         started = false;
         rs._read = (function(_this) {
